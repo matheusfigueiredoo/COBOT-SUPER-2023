ceca062a079a166fee52db66b61722b961dd2df4
eumatheusfig <eumatheusfigueiredo@gmail.com>
2024-04-07 01:44:22 -0400

    codigos de movimentação

--- projetc/[2] Sequence_mov.py
+++ projetc/[2] Sequence_mov.py
@@ -2,41 +2,44 @@ import rtde_control
 import rtde_io
 import time
 
-rtde_io_ = rtde_io.RTDEIOInterface("10.224.2.69")
-rtde_c = rtde_control.RTDEControlInterface("10.224.2.69")
+HOST = "123"
 
+rtde_io_ = rtde_io.RTDEIOInterface(HOST)
+rtde_c = rtde_control.RTDEControlInterface(HOST)
 
-base = [-4.036092583333151, -1.3568094980767746, 2.373448912297384, -2.5869633160033167, -1.568237606679098, -2.1496198813067835]
+# positions
+pos_initial = [-4.036092583333151, -1.3568094980767746, 2.373448912297384, -2.5869633160033167, -1.568237606679098, -2.1496198813067835]
 
-a = [-4.076599899922506, -0.7863095563701172, 1.3410056273089808, -2.090026994744772, -1.4898093382464808, -2.235628906880514, 5]
+red_1 = []
+red_2 = []
+red_3 = []
+red_4 = []
+red_5 = []
+red_6 = []
 
-b = [-4.059603039418356, -0.7164358657649537, 1.383453671132223, -2.2590977154173792, -1.5111663977252405, -2.1496413389789026]
+black_1 = []
+black_2 = []
+black_3 = []
+black_4 = []
+black_5 = []
+black_6 = []
+
 
 # moviments sequency
-
-rtde_c.moveJ(base)
+rtde_c.moveJ(pos_initial)
 time.sleep(1)
-rtde_c.moveJ(a)
-rtde_c.moveJ(b)
-rtde_io_.setToolDigitalOut(1, True)
-time.sleep(0.5)
-rtde_c.moveJ(a)
-rtde_c.moveJ(base)
 
-
-# to place
-
-bef_a1 =  [-2.9020732084857386, -1.077377275829651, 1.804741684590475, -2.3604818783202113, -1.5067532698260706, -2.5751686731921595] 
-a1 =  [-2.9105411211596888, -0.9263118666461487, 1.8049700895892542, -2.4291798077025355, -1.5006964842425745, -2.5751927534686487]
-
-
-
-rtde_c.moveJ(bef_a1)
-rtde_c.moveJ(a1)
-time.sleep(0.5)
+# manter a garra na posicao inicial
 rtde_io_.setToolDigitalOut(1, False)
 rtde_io_.setToolDigitalOut(0, True)
-time.sleep(0.5)
-rtde_c.moveJ(bef_a1)
-rtde_c.moveJ(base)
-rtde_io_.setToolDigitalOut(0, False)
+
+# sequencia dos movimentos de acordo com a chegada 
+# das pecas
+
+# pecas vermelhas
+# place red eh a variavel que conta as pecas armazenadas
+place_red = 0
+
+if place_red == 0:
+    rtde_c.moveJ(red_1)
+    place_red = place_red + 1

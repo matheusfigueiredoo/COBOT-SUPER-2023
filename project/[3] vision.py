7b4df2740383c39f4c90f0c73ad96c6fcaea67fd
Matheus Figueiredo <158324641+matheusfigueiredoo@users.noreply.github.com>
2024-04-12 16:34:05 -0400

    Update [3] vision.py

--- project/[3] vision.py
+++ project/[3] vision.py
@@ -54,10 +54,10 @@ def main():
     cap = cv2.VideoCapture(0)
 
     # Carregar imagens de referência e calcular histogramas
-    reference_image_red = cv2.imread('foto_webcam_red.jpg')
+    reference_image_red = cv2.imread('foto_webcam_red.png')
     reference_hist_red = calculate_histogram1(reference_image_red)
 
-    reference_image_black = cv2.imread('foto_webcam_black.jpg')
+    reference_image_black = cv2.imread('foto_webcam_black.png')
     reference_hist_black = calculate_histogram1(reference_image_black)
 
     while True:

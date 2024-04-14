dae004ce3e18f3e50ad66c225167bbba47988966
eumatheusfig <eumatheusfigueiredo@gmail.com>
2024-04-14 02:37:34 -0400

    Código de rotina do braço

--- project/[2] Sequence_mov.py
+++ project/[2] Sequence_mov.py
@@ -40,7 +40,6 @@ black_4 = []
 black_5 = []
 black_6 = []
 
-
 # moviments sequency
 time.sleep(1)
 
@@ -53,7 +52,6 @@ rtde_io_.setToolDigitalOut(0, True)
 
 # pecas vermelhas
 # memory red eh a variavel que conta as pecas armazenadas
-
 
 if memory_red == 0:
     rtde_c.moveJ(pos_ref_pick)
@@ -74,47 +72,206 @@ if memory_red == 0:
     rtde_c.moveJ(pos_ref_pick)
 
 if memory_red == 1:
+    rtde_c.moveJ(pos_ref_pick)
+    rtde_c.moveJ(pos_pick)
+    time.sleep(1)
+    rtde_io_.setToolDigitalOut(1, True)
+    time.sleep(1)
+    rtde_c.moveJ(pos_ref_pick)
+    rtde_c.moveJ(pos_ref_base)
+    rtde_c.moveJ(ref_red_2)
     rtde_c.moveJ(red_2)
+    time.sleep(1)
+    rtde_io_.setToolDigitalOut(1, False)
+    rtde_io_.setToolDigitalOut(0, True)
+    time.sleep(1)
+    rtde_c.moveJ(ref_red_2)
+    rtde_c.moveJ(pos_ref_base)
+    rtde_c.moveJ(pos_ref_pick)
     
-
 if memory_red == 2:
+    rtde_c.moveJ(pos_ref_pick)
+    rtde_c.moveJ(pos_pick)
+    time.sleep(1)
+    rtde_io_.setToolDigitalOut(1, True)
+    time.sleep(1)
+    rtde_c.moveJ(pos_ref_pick)
+    rtde_c.moveJ(pos_ref_base)
+    rtde_c.moveJ(ref_red_3)
     rtde_c.moveJ(red_3)
+    time.sleep(1)
+    rtde_io_.setToolDigitalOut(1, False)
+    rtde_io_.setToolDigitalOut(0, True)
+    time.sleep(1)
+    rtde_c.moveJ(ref_red_3)
+    rtde_c.moveJ(pos_ref_base)
+    rtde_c.moveJ(pos_ref_pick)
     
-
 if memory_red == 3:
+    rtde_c.moveJ(pos_ref_pick)
+    rtde_c.moveJ(pos_pick)
+    time.sleep(1)
+    rtde_io_.setToolDigitalOut(1, True)
+    time.sleep(1)
+    rtde_c.moveJ(pos_ref_pick)
+    rtde_c.moveJ(pos_ref_base)
+    rtde_c.moveJ(ref_red_4)
     rtde_c.moveJ(red_4)
+    time.sleep(1)
+    rtde_io_.setToolDigitalOut(1, False)
+    rtde_io_.setToolDigitalOut(0, True)
+    time.sleep(1)
+    rtde_c.moveJ(ref_red_4)
+    rtde_c.moveJ(pos_ref_base)
+    rtde_c.moveJ(pos_ref_pick)
     
-
 if memory_red == 4:
+    rtde_c.moveJ(pos_ref_pick)
+    rtde_c.moveJ(pos_pick)
+    time.sleep(1)
+    rtde_io_.setToolDigitalOut(1, True)
+    time.sleep(1)
+    rtde_c.moveJ(pos_ref_pick)
+    rtde_c.moveJ(pos_ref_base)
+    rtde_c.moveJ(ref_red_5)
     rtde_c.moveJ(red_5)
+    time.sleep(1)
+    rtde_io_.setToolDigitalOut(1, False)
+    rtde_io_.setToolDigitalOut(0, True)
+    time.sleep(1)
+    rtde_c.moveJ(ref_red_5)
+    rtde_c.moveJ(pos_ref_base)
+    rtde_c.moveJ(pos_ref_pick)
     
-
 if memory_red == 5:
+    rtde_c.moveJ(pos_ref_pick)
+    rtde_c.moveJ(pos_pick)
+    time.sleep(1)
+    rtde_io_.setToolDigitalOut(1, True)
+    time.sleep(1)
+    rtde_c.moveJ(pos_ref_pick)
+    rtde_c.moveJ(pos_ref_base)
+    rtde_c.moveJ(ref_red_6)
     rtde_c.moveJ(red_6)
+    time.sleep(1)
+    rtde_io_.setToolDigitalOut(1, False)
+    rtde_io_.setToolDigitalOut(0, True)
+    time.sleep(1)
+    rtde_c.moveJ(ref_red_6)
+    rtde_c.moveJ(pos_ref_base)
+    rtde_c.moveJ(pos_ref_pick)
     
 memory_red = memory_red + 1
 
+# pecas pretas
+# memory black eh a variavel que conta as pecas armazenadas
+
 if memory_black == 0:
+    rtde_c.moveJ(pos_ref_pick)
+    rtde_c.moveJ(pos_pick)
+    time.sleep(1)
+    rtde_io_.setToolDigitalOut(1, True)
+    time.sleep(1)
+    rtde_c.moveJ(pos_ref_pick)
+    rtde_c.moveJ(pos_ref_base)
+    rtde_c.moveJ(ref_black_1)
     rtde_c.moveJ(black_1)
-    
+    time.sleep(1)
+    rtde_io_.setToolDigitalOut(1, False)
+    rtde_io_.setToolDigitalOut(0, True)
+    time.sleep(1)
+    rtde_c.moveJ(ref_black_1)
+    rtde_c.moveJ(pos_ref_base)
+    rtde_c.moveJ(pos_ref_pick)   
 
 if memory_black == 1:
+    rtde_c.moveJ(pos_ref_pick)
+    rtde_c.moveJ(pos_pick)
+    time.sleep(1)
+    rtde_io_.setToolDigitalOut(1, True)
+    time.sleep(1)
+    rtde_c.moveJ(pos_ref_pick)
+    rtde_c.moveJ(pos_ref_base)
+    rtde_c.moveJ(ref_black_2)
     rtde_c.moveJ(black_2)
-    
+    time.sleep(1)
+    rtde_io_.setToolDigitalOut(1, False)
+    rtde_io_.setToolDigitalOut(0, True)
+    time.sleep(1)
+    rtde_c.moveJ(ref_black_2)
+    rtde_c.moveJ(pos_ref_base)
+    rtde_c.moveJ(pos_ref_pick)  
 
 if memory_black == 2:
+    rtde_c.moveJ(pos_ref_pick)
+    rtde_c.moveJ(pos_pick)
+    time.sleep(1)
+    rtde_io_.setToolDigitalOut(1, True)
+    time.sleep(1)
+    rtde_c.moveJ(pos_ref_pick)
+    rtde_c.moveJ(pos_ref_base)
+    rtde_c.moveJ(ref_black_3)
     rtde_c.moveJ(black_3)
+    time.sleep(1)
+    rtde_io_.setToolDigitalOut(1, False)
+    rtde_io_.setToolDigitalOut(0, True)
+    time.sleep(1)
+    rtde_c.moveJ(ref_black_3)
+    rtde_c.moveJ(pos_ref_base)
+    rtde_c.moveJ(pos_ref_pick)
     
-
 if memory_black == 3:
+    rtde_c.moveJ(pos_ref_pick)
+    rtde_c.moveJ(pos_pick)
+    time.sleep(1)
+    rtde_io_.setToolDigitalOut(1, True)
+    time.sleep(1)
+    rtde_c.moveJ(pos_ref_pick)
+    rtde_c.moveJ(pos_ref_base)
+    rtde_c.moveJ(ref_black_4)
     rtde_c.moveJ(black_4)
+    time.sleep(1)
+    rtde_io_.setToolDigitalOut(1, False)
+    rtde_io_.setToolDigitalOut(0, True)
+    time.sleep(1)
+    rtde_c.moveJ(ref_black_4)
+    rtde_c.moveJ(pos_ref_base)
+    rtde_c.moveJ(pos_ref_pick)
     
-
 if memory_black == 4:
+    rtde_c.moveJ(pos_ref_pick)
+    rtde_c.moveJ(pos_pick)
+    time.sleep(1)
+    rtde_io_.setToolDigitalOut(1, True)
+    time.sleep(1)
+    rtde_c.moveJ(pos_ref_pick)
+    rtde_c.moveJ(pos_ref_base)
+    rtde_c.moveJ(ref_black_5)
     rtde_c.moveJ(black_5)
+    time.sleep(1)
+    rtde_io_.setToolDigitalOut(1, False)
+    rtde_io_.setToolDigitalOut(0, True)
+    time.sleep(1)
+    rtde_c.moveJ(ref_black_5)
+    rtde_c.moveJ(pos_ref_base)
+    rtde_c.moveJ(pos_ref_pick)
     
-
 if memory_black == 5:
+    rtde_c.moveJ(pos_ref_pick)
+    rtde_c.moveJ(pos_pick)
+    time.sleep(1)
+    rtde_io_.setToolDigitalOut(1, True)
+    time.sleep(1)
+    rtde_c.moveJ(pos_ref_pick)
+    rtde_c.moveJ(pos_ref_base)
+    rtde_c.moveJ(ref_black_6)
     rtde_c.moveJ(black_6)
+    time.sleep(1)
+    rtde_io_.setToolDigitalOut(1, False)
+    rtde_io_.setToolDigitalOut(0, True)
+    time.sleep(1)
+    rtde_c.moveJ(ref_black_6)
+    rtde_c.moveJ(pos_ref_base)
+    rtde_c.moveJ(pos_ref_pick)
     
 memory_black = memory_black + 1

cd446925fd4d98d7964c77e42d08acab35ef37c5
eumatheusfig <eumatheusfigueiredo@gmail.com>
2024-04-15 10:57:32 -0400

    novo

--- project/[1] Get_actual.py
+++ project/[1] Get_actual.py
@@ -1,7 +1,6 @@
 import rtde_receive
-rtde_r = rtde_receive.RTDEReceiveInterface("10.224.2.69")
+rtde_r = rtde_receive.RTDEReceiveInterface("10.224.2.60")
 
 actual_q = rtde_r.getActualQ()
-
 
 print("a = ", actual_q)
--- project/main.py
+++ project/main.py
@@ -3,3 +3,13 @@
 
 import move
 
+# as variáveis memory red/black são utilizadas para indicar que a posição 
+# está ocupada.
+
+# elas são incrementadas assim que o robô coloca a peça na posição.
+
+# exemplo: se a memória estiver com valor 0, o robô coloca na posição 1 e incrementa a variável.
+# se estiver com valor 1, o robô coloca na posição 2 e incrementa.
+memory_red = 0
+memory_black = 0
+

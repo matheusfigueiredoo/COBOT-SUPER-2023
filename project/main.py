9041663a2e0c7f30c6a32c51676d06c88004f4ef
eumatheusfig <eumatheusfigueiredo@gmail.com>
2024-04-16 13:02:29 -0400

    .

--- project/main.py
+++ project/main.py
@@ -7,9 +7,7 @@ import numpy as np
 
 # as variáveis memory red/black são utilizadas para indicar que a posição 
 # está ocupada.
-
 # elas são incrementadas assim que o robô coloca a peça na posição.
-
 # exemplo: se a memória estiver com valor 0, o robô coloca na posição 1 e incrementa a variável.
 # se estiver com valor 1, o robô coloca na posição 2 e incrementa.
 

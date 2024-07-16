a26d9e63c62c9f772c8e8b54b230f4516f9e0e90
Matheus Figueiredo <158324641+matheusfigueiredoo@users.noreply.github.com>
2024-07-16 17:38:41 -0400

    Update histogram_black.py
    Signed-off-by: Matheus Figueiredo <158324641+matheusfigueiredoo@users.noreply.github.com>

--- histogram_black.py
+++ histogram_black.py
@@ -3,10 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def plot_histogram(image):
-    # Calcula o histograma da imagem em escala de cinza
     histogram = cv2.calcHist([image], [0], None, [256], [0, 256])
-    
-    # Plota o histograma
     plt.plot(histogram, color='gray')
     plt.xlabel('Gray level')
     plt.ylabel('Pixels')
@@ -14,20 +11,11 @@ def plot_histogram(image):
     plt.show()
 
 def main():
-    # Carrega a imagem
     image = cv2.imread('project/foto_webcam_black.jpg')
-
-    # Converte a imagem para escala de cinza
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # Mostra a imagem em escala de cinza
     cv2.imshow('Image', gray_image)
     cv2.waitKey(0)
-    
-    # Plota o histograma da imagem em escala de cinza
     plot_histogram(gray_image)
-
-    # Fecha todas as janelas
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
